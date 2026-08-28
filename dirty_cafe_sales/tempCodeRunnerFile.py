9fef23343ac21afd6d896595cdb05a0d85a32787
Fenil Patel <fenil.patel0706@gmail.com>
2026-08-28 21:54:25 +0530

    adjust folder structure

--- dirty_cafe_sales/tempCodeRunnerFile.py
+++
@@ -1,4 +0,0 @@
-
-# df['number_of_reviews'] =pd.to_numeric(df['number_of_reviews'],errors='coerce')
-# df['number_of_reviews'] =df['number_of_reviews'].fillna(0)
-# df['number_of_reviews'] =df['number_of_reviews'].astype('Int64')
